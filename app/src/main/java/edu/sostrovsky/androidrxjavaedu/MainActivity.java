e6251868fc9900089569df7840b7e7d23a8a6f49
soaedu <sergey.ostrovsky.edu@gmail.com>
2020-10-07 11:51:28 +0200

    Add new icon and first example

--- app/src/main/java/edu/sostrovsky/androidrxjavaedu/MainActivity.java
+++ app/src/main/java/edu/sostrovsky/androidrxjavaedu/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.reactivex.Observable;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> observable = Observable.just("Hello World");
+        observable.subscribe(System.out::println);
     }
 }
